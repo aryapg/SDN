97c008cf3d4daa8cc1250171931998d8ed7b5d2d
AryaPG <110161253+aryapg@users.noreply.github.com>
2025-08-19 10:11:20 +0530

    lab1

--- svm.py
+++ svm.py
@@ -2,9 +2,20 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.svm import SVC
-X = [[30],[40],[50],[60],[20],[10],[70]]
-y = [0,1,1,1,0,0,1]
-classifier = SVC(kernel = 'linear', random_state = 0)
-classifier.fit(X,y)
-X_marks=[[55]]
-print(classifier.predict(X_marks))
+
+# Training data
+X = [[30], [40], [50], [60], [20], [10], [70]]
+y = [0, 1, 1, 1, 0, 0, 1]
+
+# Train the model
+classifier = SVC(kernel="linear", random_state=0)
+classifier.fit(X, y)
+
+def main():
+    """Run a sample prediction"""
+    X_marks = [[55]]
+    prediction = classifier.predict(X_marks)
+    print("Prediction for 55:", prediction[0])
+
+if __name__ == "__main__":
+    main()
